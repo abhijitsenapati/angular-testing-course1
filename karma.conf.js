96e4a2786c29db94df32dab4e59f468a7421497c
abhijitsenapati <97079215+abhijitsenapati@users.noreply.github.com>
2022-01-09 16:34:09 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
       subdir:'coverage',
       file: 'code-coverage.xml'
     }
-    reporters: ['progress', 'kjhtml', 'coverage', 'junit'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
